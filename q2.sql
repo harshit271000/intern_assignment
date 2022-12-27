ada45f08cd8382e0ffed352706730cbf713dabe9
harshit271000 <96373424+harshit271000@users.noreply.github.com>
2022-12-27 16:59:00 +0530

    Update q2.sql

--- q2.sql
+++ q2.sql
@@ -1,4 +1,13 @@
+--a)
 select TCONST, PRIMARYTITLE, RUNTIMEMINUTES, GENRES from movies
 order by RUNTIMEMINUTES desc
 fetch next 10 rows only;
 
+--b)
+insert into movies values ('','','',,'');
+
+--c)
+select ratings.TCONST, movies.primaryTitle, movies.GENRES, ratings.AVERAGERATING from ratings, movies 
+where movies.TCONST = ratings.TCONST 
+and AVERAGERATING >6
+order by AVERAGERATING;
